49da0beff630e471be06c31e691177c074ffa12c
cybercode250 <45370267+cybercode250@users.noreply.github.com>
2018-11-27 10:20:22 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>User registration system using PHP and MySQL</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+  <form method="post" action="login.php">
+    <div class="input-group">
+      <label>Username</label>
+      <input type="text" name="username">
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password_1">
+    </div>
+    
+    <div class="input-group">
+      <button type="submit" name="login" class="btn">Register</button>
+    </div>
+    <p>
+    	Not yet a member?<a href="register.php">Sign Up </a>
+    </p>
+  <form/>
+</body>
+</html>
